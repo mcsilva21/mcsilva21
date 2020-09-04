83fb86e842dca2ac6422fb644b56672bd5d1c927
DESKTOP-HQ563S9\GKY <mcsilva21@naver.com>
2020-09-04 16:03:44 +0900

    0904 삭제

--- project/BookShop/src/main/java/kr/ac/kopo/bookshop/service/CustomerService.java
+++
@@ -1,9 +0,0 @@
-package kr.ac.kopo.bookshop.service;
-
-import kr.ac.kopo.bookshop.model.Customer;
-
-public interface CustomerService {
-
-	boolean login(Customer item);
-
-}
--- project20200902/BookShop/src/main/java/kr/ac/kopo/bookshop/dao/CustomerDao.java
+++
@@ -1,9 +0,0 @@
-package kr.ac.kopo.bookshop.dao;
-
-import kr.ac.kopo.bookshop.model.Customer;
-
-public interface CustomerDao {
-
-	int login(Customer item);
-
-}
